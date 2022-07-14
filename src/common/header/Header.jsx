5ebf44b3e7ac9aa4149be6526d5a0e9c1a65b72a
Your Nameclear <you@example.com>
2022-07-14 04:38:47 +0200

    create new priject

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from './Head'
+import Navbar from './Navbar'
+import Search from './Search'
+import './Header.css'
+const Header = ({ CartItem }) => {
+  return (
+    <div>
+<Head/>
+<Search CartItem={CartItem} />
+<Navbar/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/mainpage/Slider.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import SlidCard from "./SlidCard"
+
+const Slider = () => {
+  return (
+    <>
+      <section className='homeSlide contentWidth'>
+        <div className='container'>
+          <SlidCard />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Slider
