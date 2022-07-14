55868841c21c8df47597c285eec911b0d8a03264
Your Nameclear <you@example.com>
2022-07-14 08:17:24 +0200

    up

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,44 +1,49 @@
+const URL=`${process.env.URL_PUBLIC}`
+console.log(
+
+process.env.REACT_APP_PUBLIC_URL
+)
 const Data = {
-    productItems: [
-      {
+  productItems: [
+    {
         id: 1,
         discount: 50,
-        cover: `${process.env.URL}/images/flash/flash-1.png`,
+        cover: `${process.env.REACT_APP_PUBLIC_URL}/images/flash/flash-1.png`,
         name: "Shoes",
         price: 100,
       },
       {
         id: 2,
         discount: 40,
-        cover: `${process.env.URL}./images/flash/flash-2.png`,
+        cover: `${process.env.REACT_APP_PUBLIC_URL}./images/flash/flash-2.png`,
         name: "Watch",
         price: 20,
       },
       {
         id: 3,
         discount: 40,
-        cover: `${process.env.URL}/images/flash/flash-3.png`,
+        cover: `${process.env.REACT_APP_PUBLIC_URL}/images/flash/flash-3.png`,
         name: "Smart Mobile Black",
         price: 200,
       },
       {
         id: 4,
         discount: 40,
-        cover: `${process.env.URL}/images/flash/flash-4.png`,
+        cover: `${process.env.REACT_APP_PUBLIC_URL}/images/flash/flash-4.png`,
         name: "Smart Watch Black",
         price: 50,
       },
       {
         id: 5,
         discount: 50,
-        cover: `${process.env.URL}/images/flash/flash-1.png`,
+        cover: `${process.env.REACT_APP_PUBLIC_URL}/images/flash/flash-1.png`,
         name: "Shoes",
         price: 100,
       },
       {
         id: 6,
         discount: 50,
-        cover: `${process.env.URL}/images/flash/flash-3.png`,
+        cover: `${process.env.REACT_APP_URL_PUBLIC}/images/flash/flash-3.png`,
         name: "Shoes",
         price: 100,
       },
